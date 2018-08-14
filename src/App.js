91e264980aae86b703c0ad0a32a17c2b7a1f2da1
Tyby <tibofenyvesi84@gmail.com>
2018-08-14 11:28:19 +0100

    Application is running

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Persons from './containers/Persons';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Persons />
+      </div>
+    );
+  }
+}
+
+export default App;
